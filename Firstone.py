4c7184eb6dc5d0236ca79f10e08e9d34835de029
RAMPUR <arampur@github.com>
2018-03-24 10:21:19 -0400

    AddedComment

--- Firstone.py
+++ Firstone.py
@@ -1,1 +1,8 @@
 print('This is first file..')
+
+#Counting numbner of lines in file
+fhand = open(readme)
+count = 0
+for line in fhand:
+    count = count + 1
+print('number of lines in file', count)
