d0dd445a9c1c4492b51aa1bb4e2ce2a858ed76e1
RAMPUR <arampur@github.com>
2018-03-25 00:00:58 -0400

    secondfilecommit

---
+++ Secondone.py
@@ -0,0 +1,12 @@
+fname = input('Enter the filename..')
+try:
+    fhand = open(fname)
+except:
+    print('File cant be opened..', fname)
+    exit()
+
+count = 0
+for line in fhand:
+    if line.startswith('Subject'):
+        count = count + 1
+    print('There were',count,'subject line in',fname)
