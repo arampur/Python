0098c0ae79b6684189ebba9045300c876df2be32
RAMPUR-MAC <arampur@csu.fullerton.edu>
2018-07-07 18:42:33 -0700

    commit

---
+++ readfile.py
@@ -0,0 +1,16 @@
+filename = 'TextFiles/firsttext.txt'
+with open(filename) as fileobject:
+    lines = fileobject.readlines()
+
+pi_string = ''
+for line in lines:
+    pi_string += line.rstrip()
+
+mynumber = input("enter the number you want to search :")
+if mynumber in pi_string:
+    print("your number is found in the file..")
+else:
+    print("sorry entered text not found..try something else..")
+
+with open('blankfile.txt','w') as writefile:
+    writefile.write(mynumber)
---
+++ readtxt.py
@@ -0,0 +1,3 @@
+with open('TextFiles/firsttext.txt') as file_object:
+    contents = file_object.read()
+    print(contents)
